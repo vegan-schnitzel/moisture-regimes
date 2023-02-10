4d3185e77dfee7e5905a5903b54d042d3794a1e9
Robert Wright <rw0064fu@zedat.fu-berlin.de>
2023-02-10 17:56:53 +0100

    re-structured analysis

--- data/preproc-pre.sh
+++ data/preproc-pre.sh
@@ -6,11 +6,18 @@ CDO_FLGS="-L -O -f nc4"
 
 for SIM in "SSP1" "SSP3" "SSP5" "plasim_ref_ln"
 # reference sim in ingokir area
+
 do
+
     DATA="/home/rw0064fu/models/project/exps/${SIM}/ANALYSIS/atm/daily/sfc/code_009"
-    # compute (time mean of) climatology on 5-day means
-    cdo $CDO_FLGS -timavg -ydayavg -timselavg,5 -del29feb -selyear,20/29 $DATA ${SIM}-pre-clim.nc
-    cdo $CDO_FLGS -timmax -ydaystd -timselavg,5 -del29feb -selyear,20/29 $DATA ${SIM}-pre-clim-std.nc
+    
+    # compute climatology on 5-day means
+    cdo $CDO_FLGS -ydayavg -timselavg,5 -del29feb -selyear,20/29 $DATA ${SIM}-pre-clim.nc
+    cdo $CDO_FLGS -ydaystd -timselavg,5 -del29feb -selyear,20/29 $DATA ${SIM}-pre-clim-std.nc
+
+    # compute annual precipitation sums (for last years)
+    cdo $CDO_FLGS -yearsum -del29feb -selyear,20/29 $DATA ${SIM}-pre-annual.nc
+    
 done
 
 # compute anomaly
--- data/preproc-tmp.sh
+++ data/preproc-tmp.sh
@@ -4,12 +4,18 @@ CDO_FLGS="-L -O -f nc4"
 
 # 2m air temperature = 167
 
-for SIM in "SSP1" "SSP3" "SSP5" "plasim_ref_ln"
 # reference sim in ingokir area
+for SIM in "SSP1" "SSP3" "SSP5" "plasim_ref_ln"
 do
+
     DATA="/home/rw0064fu/models/project/exps/${SIM}/ANALYSIS/atm/daily/sfc_gmean/code_167"
 
     # compute climatology on 5-day means
     cdo $CDO_FLGS -ydayavg -timselavg,5 -del29feb -selyear,20/29 $DATA ${SIM}-tmp-clim.nc
     cdo $CDO_FLGS -ydaystd -timselavg,5 -del29feb -selyear,20/29 $DATA ${SIM}-tmp-clim-std.nc
+
+    # compute annual mean temperature
+    cdo $CDO_FLGS -timavg -yearavg -del29feb -selyear,20/29 $DATA ${SIM}-tmp-annual.nc
+    cdo $CDO_FLGS -timstd -yearavg -del29feb -selyear,20/29 $DATA ${SIM}-tmp-annual-std.nc
+
 done
